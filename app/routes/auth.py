de10a26e2ff540bf29d9c13bd37373cfc8962faf
Pouya Gharibpour <p.gharibpour@gmail.com>
2025-04-18 10:04:41 +0330

    add user signup route

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -4,22 +4,41 @@ from sqlalchemy.orm import Session
 
 
 from ..utils.auth import (get_api_key,
+                          hash_password,
                           get_current_user,
                           authenticate_user,
                           create_access_token,
                           get_current_active_user,
                           ACCESS_TOKEN_EXPIRE_MINUTES)
 from ..database.db import get_db
-from ..database.models import User
+from ..database.models import User, APIKey
 
-from typing import Annotated
-from datetime import timedelta
+from typing import Annotated, Union
+from datetime import timedelta, datetime
+import secrets
 
 router = APIRouter()
 
 @router.get("/request")
 async def status():
     return {"message": "Auth router is working!"}
+
+@router.post("/users/new")
+async def user_signup(username: str, password: str, email: str, db: Annotated[Session, Depends(get_db)]):
+    username_check = db.query(User).filter(User.username == username or User.email == email).first()
+    if username_check:
+        raise HTTPException(
+            status_code=401,
+            detail="Username/email is already taken. Use a different one."
+        )
+    new_user = User(username=username, email=email, hashed_password=hash_password(password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return new_user
+
+
 
 @router.post("/token", tags=["authorization"])
 def login_for_access_token(
@@ -36,17 +55,18 @@ def login_for_access_token(
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     scopes = []
-    if user.is_superuser:
-        scopes = ["super"]
+    # if user.is_superuser:
+    #     scopes = ["super"]
     access_token = create_access_token(
         data={"sub": user.username, "scopes": scopes},
         expires_delta=access_token_expires
         )
     return {"access_token": access_token, "token_type":"bearer"}
 
-@router.get("/users/me/", response_model=User, tags=["users"])
+@router.get("/users/me/", tags=["users"])
 def get_current_logged_in_user(
         current_user: Annotated[User, Depends(get_current_active_user)]
     ):
 
     return current_user
+
