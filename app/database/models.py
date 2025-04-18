75079dc82685e6839443a76ff80e43b6f94a12fe
Pouya Gharibpour <p.gharibpour@gmail.com>
2025-04-18 10:02:30 +0330

    fix db relationships

--- app/database/models.py
+++ app/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Enum, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Enum, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
@@ -22,7 +22,7 @@ class User(Base):
     is_active = Column(Integer, default=1)
     role = Column(Enum(RoleEnum), default=RoleEnum.normal, nullable=False)
 
-    api_secrets = relationship("APISecret", backref="owner", cascade="all, delete-orphan")
+    api_keys = relationship("APIKey", backref="owner", cascade="all, delete-orphan")
 
 
 class APIKey(Base):
@@ -31,10 +31,9 @@ class APIKey(Base):
     id = Column(Integer, primary_key=True, index=True)
     key = Column(String, unique=True, nullable=False)
     owner_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    is_active = Column(Integer, default=1)
+    is_active = Column(Boolean, default=True)
     expiration_date = Column(DateTime, nullable=True)
 
-    owner = relationship("User", back_populates="api_secrets")
     tasks = relationship("Task", backref="api_key", cascade="all, delete-orphan")
 
 
@@ -47,11 +46,9 @@ class Task(Base):
         failed = "failed"
 
     id = Column(Integer, primary_key=True, index=True)
-    filename = Column(String, unique=True, nullable=False)
+    filename = Column(String, nullable=False)
     api_key_id = Column(Integer, ForeignKey("api_keys.id"), nullable=False, index=True)
     state = Column(Enum(StateEnum), default=StateEnum.processing, nullable=False, index=True)
     created_at = Column(DateTime, server_default=func.now(), nullable=False)
     updated_at = Column(DateTime, server_default=func.now(), server_onupdate=func.now(), nullable=False)
     result = Column(Integer, default=-1)
-
-    api_key = relationship("APIKey", back_populates="tasks")
