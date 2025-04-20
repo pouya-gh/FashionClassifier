dc8318128a7e05ca97efbc7671881b223719f759
Pouya Gharibpour <p.gharibpour@gmail.com>
2025-04-20 10:17:10 +0330

    add a get_current_admin_user dependency

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -125,6 +125,13 @@ def get_current_user(
             )
     return user
 
+def get_current_admin_user(
+        current_user: Annotated[User, Depends(get_current_user)]
+        ):
+    if current_user.role != User.RoleEnum.admin:
+        raise HTTPException(status_code=403, detail="You are unauthorized to use this feature.")
+    return current_user
+
 def get_current_active_user(
         current_user: Annotated[User, Depends(get_current_user)]
         ):
