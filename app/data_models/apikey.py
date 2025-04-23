4f8effd4a9a7ff04ccab7b4c3eff08d2c7e626ec
Pouya Gharibpour <p.gharibpour@gmail.com>
2025-04-23 09:30:18 +0330

    add admins' apikey api

--- app/data_models/apikey.py
+++ app/data_models/apikey.py
@@ -5,6 +5,15 @@ from datetime import datetime
 class APIKey(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
+    id: int
     key: str
     is_active: bool
     expiration_date: datetime
+
+class APIKeyAdmin(APIKey):
+    owner_id: int
+
+class APIKeyUpdate(BaseModel):
+    key: str | None = None
+    is_active: bool | None = None
+    expiration_date: datetime | None = None
---
+++ app/routes/admin/apikeys.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter, HTTPException, Depends
+
+from app.database.models import APIKey
+from app.database.db import get_db
+from app.data_models import apikey as apikey_dm
+from app.utils.auth import get_current_admin_user
+
+router = APIRouter(prefix="/admin",
+                   tags=["admin", "apikeys"],
+                   dependencies=[Depends(get_current_admin_user)])
+
+@router.get("/apikeys", response_model=list[apikey_dm.APIKeyAdmin])
+async def get_apikeys(
+    skip: int = 0, 
+    limit: int = 10, 
+    is_active: bool = None,
+    db=Depends(get_db)
+):
+    query = db.query(APIKey)
+    
+    if is_active is not None:
+        query = query.filter(APIKey.is_active == is_active)
+    
+    apikeys = query.offset(skip).limit(limit).all()
+    if not apikeys:
+        raise HTTPException(status_code=404, detail="No API keys found")
+    return apikeys
+
+@router.get("/apikeys/{apikey_id}", response_model=apikey_dm.APIKeyAdmin)
+async def get_apikey(
+    apikey_id: int, 
+    db=Depends(get_db)
+):
+    apikey = db.query(APIKey).filter(APIKey.id == apikey_id).first()
+    if not apikey:
+        raise HTTPException(status_code=404, detail="API key not found")
+    return apikey
+
+@router.patch("/apikeys/{apikey_id}")
+async def update_apikey(
+    apikey_id: int,
+    apikey_update: apikey_dm.APIKeyUpdate,
+    db=Depends(get_db)
+):
+    update_data = apikey_update.model_dump(exclude_none=True)
+    result = db.query(APIKey).filter(APIKey.id == apikey_id).update(update_data)
+    db.commit()
+    if result > 0:
+        return {"message": "API key info updated"}
+    else:
+        raise HTTPException(404, f"API key with id {apikey_id} not found")
+
+@router.delete("/apikeys/{apikey_id}", status_code=204)
+async def delete_apikey(
+    apikey_id: int,
+    db=Depends(get_db)
+):
+    apikey = db.query(APIKey).filter(APIKey.id == apikey_id).first()
+    if not apikey:
+        raise HTTPException(status_code=404, detail="API key not found")
+    db.delete(apikey)
+    db.commit()
+    return {"message": f"API key {apikey_id} deleted."}
