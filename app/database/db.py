d70e20e3b7a8d3ca8f57f9676f2e19960ad0b27a
Pouya Gharibpour <p.gharibpour@gmail.com>
2025-04-24 09:27:05 +0330

    load the correct db using the "ENVIRONMENT" env var

--- app/database/db.py
+++ app/database/db.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Database configuration
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+import os
+
+if os.getenv("ENVIRONMENT", default="dev") == "test":
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_test.db"
+else:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+# # Database configuration
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
