07ea29a09491a877803d43dbc9823af464fe4761
Pouya Gharibpour <p.gharibpour@gmail.com>
2025-05-31 11:48:28 +0330

    send classification task done message to redis for notification purposes

--- app/tasks.py
+++ app/tasks.py
@@ -1,6 +1,12 @@
 from app.database.db import get_db
 from app.database.models import Task
 from celery import Celery
+import redis
+import json
+
+from app.config import (REDIS_DB,
+                        REDIS_HOST,
+                        REDIS_PORT)
 
 from app.utils.classifier import classify_image, FAHION_MNIST_CLASS_NAMES
 
@@ -10,6 +16,7 @@ import os
 
 app = Celery('tasks', broker=CELERY_BROKER, backend=CELERY_BACKEND)
 
+redis_connection = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
 
 @app.task
 def classify_task(task_id: int):
@@ -23,10 +30,19 @@ def classify_task(task_id: int):
     task = db.query(Task).filter(Task.id == task_id).first()
     if not task:
         return
+    
+    message_queue = f"taskmessages:{task.user_id}"
+
     print(f"Processing file in the background: {task.filename}")
     result = classify_image(task.filename)
     task.result = result
     task.state = Task.StateEnum.done
     db.commit()
+    message = {
+        "event": "classification_completed",
+        "retry": 3000,
+        "data": f"Task {task.id} finished. Result: {FAHION_MNIST_CLASS_NAMES[result]}({result})",
+    }
+    redis_connection.lpush(message_queue, json.dumps(message))
     print(f"Classification arg: {result}, ({FAHION_MNIST_CLASS_NAMES[result]})")
     os.remove(task.filename)
